1801af0ccca014057adabf9959375edf633a9d36
CinLo82 <cinlos3382@gmail.com>
2023-09-22 16:09:58 -0300

    .module.css

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "../Navbar/Navbar"
+import style from './layout.module.css'
 
 type Props = {
     children: React.ReactNode;
@@ -6,7 +7,7 @@ type Props = {
 
 const Layout: React.FC<Props> = ({ children }) => {
     return (
-        <div className="container">
+        <div className={style.container}>
             <Navbar />
             {children}
             <footer>This is the footer</footer>
